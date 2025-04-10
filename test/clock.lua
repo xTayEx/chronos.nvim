a764532af17c90c79c4195b30dd9971222ed274e
xtayex <liyuliang2022_sei@stu.ecnu.edu.cn>
2025-04-10 22:10:51 +0800

    style: indent in test.

--- test/clock.lua
+++ test/clock.lua
@@ -1,7 +1,7 @@
 local Clock = require("chronos.clock").Clock
 
-describe("Create new Clock instance", function ()
-  it("work as expect", function ()
+describe("Create new Clock instance", function()
+  it("work as expect", function()
     local clock = Clock:new()
     assert.is.truthy(clock)
   end)
